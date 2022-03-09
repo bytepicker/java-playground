69f0da6f76793ae3fa10a35132a6094baf65d7ca
bytepicker <sasha.p@hush.com>
2022-03-09 09:30:15 +0300

    screenshots edited, fibonacci added, sqrt done

--- src/main/java/com/test/fibo/Main.java
+++ src/main/java/com/test/fibo/Main.java
@@ -1,7 +1,10 @@
 package com.test.fibo;
 
 public class Main {
-    public static int fibo(int n){
+    public static int fibo(int inputNumber){
+        if(inputNumber < 0) { throw new RuntimeException("Positive input only"); }
 
+        if (inputNumber == 0 || inputNumber == 1) { return inputNumber; }
+        return fibo(inputNumber - 1) + fibo(inputNumber - 2);
     }
 }
